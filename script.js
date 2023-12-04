ae465d857c3207ef950c4b7e1770b8b356d1f01c
yagnik0 <yashyagnik456@.com>
2023-12-04 19:46:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+const wrappers = document.querySelectorAll('.wrapper');
+
+
+wrappers.forEach((wrapper) => {
+    const headSelc = wrapper.querySelector('.heading-style');
+    const paraSelc = wrapper.querySelector('.para-style');
+    const plusBtn = wrapper.querySelector('.icon-plus');
+    const minusBtn = wrapper.querySelector('.icon-minus');
+
+    headSelc.addEventListener('click', () => {
+        paraSelc.classList.toggle('hidden');
+        plusBtn.classList.toggle('hidden');
+        minusBtn.classList.toggle('hidden');
+    })
+})
+
