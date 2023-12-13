6cc66290226bfe3cbbd6507a6c82cc719ce9f366
yagnik0 <yashyagnik456@.com>
2023-12-13 20:02:38 +0530

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
-const wrappers = document.querySelectorAll('.wrapper');
 
+const wrappers = document.querySelectorAll('.wrapper');
 
 wrappers.forEach((wrapper) => {
     const headSelc = wrapper.querySelector('.heading-style');
@@ -8,13 +8,22 @@ wrappers.forEach((wrapper) => {
     const plusBtn = wrapper.querySelector('.icon-plus');
     const minusBtn = wrapper.querySelector('.icon-minus');
 
-    const toggleVisibility  = function() {
+    const toggleVisibility = function () {
+        wrappers.forEach((otherWrapper) => {
+            if (otherWrapper !== wrapper) {
+                otherWrapper.querySelector('.para-style').classList.add('hidden');
+                otherWrapper.querySelector('.icon-plus').classList.remove('hidden');
+                otherWrapper.querySelector('.icon-minus').classList.add('hidden');
+            }
+        });
+
         paraSelc.classList.toggle('hidden');
         plusBtn.classList.toggle('hidden');
         minusBtn.classList.toggle('hidden');
     }
 
-    headSelc.addEventListener('click', toggleVisibility)
-    plusBtn.addEventListener('click', toggleVisibility)
-    minusBtn.addEventListener('click', toggleVisibility)
+    headSelc.addEventListener('click', toggleVisibility);
+    plusBtn.addEventListener('click', toggleVisibility);
+    minusBtn.addEventListener('click', toggleVisibility);
 });
+
